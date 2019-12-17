499fd9cd821b31476d455426073d288f12726a72
robson-gomes <robson.gs2630@gmail.com>
2019-12-17 15:17:00 -0300

    Adicionando a lógica do MedicaoController

--- app/Http/Controllers/API/MedicaoController.php
+++ app/Http/Controllers/API/MedicaoController.php
@@ -5,9 +5,15 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Medicao;
+use App\API\ApiError;
 
 class MedicaoController extends Controller
 {
+    private $medicao;
+    public function __construct(Medicao $medicao)
+    {
+        $this->medicao = $medicao;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -15,7 +21,7 @@ class MedicaoController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json($this->medicao->paginate(5));
     }
 
     /**
@@ -26,7 +32,17 @@ class MedicaoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $medicaodata = $request->all();
+            $this->medicao->create($medicaodata);
+            $return = ['data' => ['msg' => 'medição criada com sucesso.']];
+            return response()->json($return, 201);
+        } catch (\Exception $e) {
+            if(config('app.debug')) {
+                return response()->json(ApiError::errorMessage($e->getMessage(), 500), 500);
+            }
+            return response()->json(ApiError::errorMessage('erro ao realizar operação de salvar', 500),  500);
+        }
     }
 
     /**
@@ -35,9 +51,13 @@ class MedicaoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Medicao $id)
     {
-        //
+        $medicao = $this->medicao::with('medicao')->find($id);
+        if(!$medicao) return response()->json(ApiError::errorMessage('medicao não encontrado', 404), 404);
+        
+        $data = ['data' => $medicao];
+        return response() -> json($data);
     }
 
     /**
@@ -49,7 +69,18 @@ class MedicaoController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            $medicaodata = $request->all();
+            $medicao = $this->medicao->find($id);
+            $medicao->update($medicaodata);
+            $return = ['data' => ['msg' => 'medição atualizada com sucesso']];
+            return response()->json($return, 201);
+        } catch (\Exception $e) {
+            if(config('app.debug')) {
+                return response()->json(ApiError::errorMessage($e->getMessage(), 500),  500);
+            }
+            return response()->json(ApiError::errorMessage('erro ao realizar operação de atualizar', 500), 500);
+        }
     }
 
     /**
@@ -58,8 +89,17 @@ class MedicaoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Medicao $id)
     {
-        //
+        try {
+            
+            $id->delete();
+            return response()-> json(['data'=> ['msg'=> 'medicao: '.$id->id. ' deletada com sucesso.']],200);
+        }catch (\Exception $e) {
+            if(config('app.debug')) {
+                return response()->json(ApiError::errorMessage($e->getMessage(), 500),  500);
+            }
+            return response()->json(ApiError::errorMessage('erro ao realizar operação de deletar', 500),  500);
+        }
     }
 }
